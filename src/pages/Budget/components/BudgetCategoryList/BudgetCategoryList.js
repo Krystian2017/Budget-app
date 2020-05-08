000e165aba54219fa3164efb10e9ead9ef511d8f
Krystian Bieniek <krystekk2013@gmail.com>
2020-05-08 13:40:05 +0200

    Add amount to CategoryItem and ParentCategory

--- src/pages/Budget/components/BudgetCategoryList/BudgetCategoryList.js
+++ src/pages/Budget/components/BudgetCategoryList/BudgetCategoryList.js
@@ -4,9 +4,9 @@ import { groupBy } from "lodash";
 
 import { ToggleableList } from "components";
 import ParentCategory from "./ParentCategory";
-import CategoryItem from "./ParentCategory";
+import CategoryItem from "./CategoryItem";
 
-function BudgetCategoryList({ budgetedCategoryies, allCategories }) {
+function BudgetCategoryList({ budgetedCategoryies, allCategories, budget }) {
   const budgetCategoriesByParent = groupBy(
     budgetedCategoryies,
     (item) =>
@@ -18,21 +18,45 @@ function BudgetCategoryList({ budgetedCategoryies, allCategories }) {
     ([parentName, categories]) => ({
       id: parentName,
       Trigger: ({ onClick }) => (
-        <ParentCategory name={parentName} onClick={() => onClick(parentName)} />
+        <ParentCategory
+          name={parentName}
+          onClick={() => onClick(parentName)}
+          categories={categories}
+          transactions={budget.transactions}
+        />
       ),
-      children: categories.map((budgetCategory) => {
+      children: categories.map((budgetedCategory) => {
         const { name } = allCategories.find(
-          (category) => category.id === budgetCategory.categoryId
+          (category) => category.id === budgetedCategory.categoryId
         );
-        return <CategoryItem key={budgetCategory.id} name={name} />;
+        return (
+          <CategoryItem
+            key={budgetedCategory.id}
+            name={name}
+            item={budgetedCategory}
+            transactions={budget.transactions}
+          />
+        );
       }),
     })
   );
 
-  return <ToggleableList items={listItems} />;
+  const totalSpent = budget.transactions.reduce(
+    (acc, transaction) => acc + transaction.amount,
+    0
+  );
+  const restToSpent = budget.totalAmount - totalSpent;
+
+  return (
+    <div>
+      <ParentCategory name={budget.name} amount={restToSpent} />
+      <ToggleableList items={listItems} />
+    </div>
+  );
 }
 
 export default connect((state) => ({
   budgetedCategoryies: state.budget.budgetedCategoryies,
   allCategories: state.common.allCategories,
+  budget: state.budget.budget,
 }))(BudgetCategoryList);
